095e261b80b05b3bc7de33915408d78ec65d4402
kiani14 <asusn43@yahoo.com>
2022-09-18 19:21:14 +0430

    up

---
+++ password.js
@@ -0,0 +1,6 @@
+const d = document;
+const eye = d.querySelector('.fa-eye');
+const eyeslash = d.querySelector('.fa-eye-slash');
+eye.addEventListener('click', () => {
+    eye.classList.add('hide');
+})
