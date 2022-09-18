f6a67ed28cdd6763ec1d09a409626bb0c01e2d90
kiani14 <asusn43@yahoo.com>
2022-09-18 20:24:16 +0430

    ok

--- password.js
+++ password.js
@@ -1,6 +1,15 @@
 const d = document;
 const eye = d.querySelector('.fa-eye');
 const eyeslash = d.querySelector('.fa-eye-slash');
+let e = true;
 eye.addEventListener('click', () => {
-    eye.classList.add('hide');
+    eye.classList.toggle("hide");
+    e = false;
+    // eyeslash.classList.add("show");
+    // alert("df");
+})
+eyeslash.addEventListener('click', () => {
+    eyeslash.classList.toggle("hide");
+    // eye.classList.add("show");
+    // alert("df");
 })
