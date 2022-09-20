2f28c8cb9eb33a2e6761e0999b4dcc7beefc67a3
kiani14 <asusn43@yahoo.com>
2022-09-20 17:26:16 +0430

    ok

--- password.js
+++ password.js
@@ -2,19 +2,29 @@ const d = document;
 const ey = d.querySelector('.eye');
 
 
-const eyeslash = d.querySelector('.ey-s');
+const eyeslash = d.querySelector('.eys');
 const eye = d.querySelector('.ey');
 const inp = d.querySelector('.inp');
 
+// ey.addEventListener('click', () => {
+//     if (inp.type == "text") {
+//         inp.type = "password";
+//         eye.style.display = "block";
+//         eyeslash.style.display = "none";
+//     }
+//     else if (inp.type == "password") {
+//         inp.type = "text";
+//         eye.style.display = "none";
+//         eyeslash.style.display = "block";
+//     }
+// })
 ey.addEventListener('click', () => {
     if (inp.type == "text") {
         inp.type = "password";
-        eye.style.display = "block";
-        eyeslash.style.display = "none";
+        ey.classList.remove("active");
     }
     else if (inp.type == "password") {
         inp.type = "text";
-        eye.style.display = "none";
-        eyeslash.style.display = "block";
+        ey.classList.add("active")
     }
 })
